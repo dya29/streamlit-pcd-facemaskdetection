3a7fd1ffef6bf70efaae7d310e39835ff2de72bd
dya29 <107823790+dya29@users.noreply.github.com>
2023-12-15 01:06:27 +0700

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -104,35 +104,36 @@ def main():
     maskNet = load_model("mask_detector.model")
 
     if use_webcam:
-        vid = cv2.VideoCapture(0)
-        while True:
-            ret, frame = vid.read()  # Baca frame dari webcam
+	    video_frame_callback()
+        # vid = cv2.VideoCapture(0)
+        # while True:
+        #     ret, frame = vid.read()  # Baca frame dari webcam
 
-                # pastikan ret adalah True (pembacaan frame berhasil)
-            if not ret:
-                break
-                # detect wajah dalam frame dan tentukan mana yang memakai masker mana yang tidak
+        #         # pastikan ret adalah True (pembacaan frame berhasil)
+        #     if not ret:
+        #         break
+        #         # detect wajah dalam frame dan tentukan mana yang memakai masker mana yang tidak
 
-            (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-                # loop lokasi wajah yang terdeteksi dan lokasi terkaitnya
-            for (box, pred) in zip(locs, preds):
-                    # unpack the bounding box and predictions
-                (startX, startY, endX, endY) = box
-                (mask, withoutMask) = pred
+        #     (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+        #         # loop lokasi wajah yang terdeteksi dan lokasi terkaitnya
+        #     for (box, pred) in zip(locs, preds):
+        #             # unpack the bounding box and predictions
+        #         (startX, startY, endX, endY) = box
+        #         (mask, withoutMask) = pred
 
-                    # tentukkan class label dan color yang akan kita gunakan membuat the bounding box and text
-                label = "Mask" if mask > withoutMask else "No Mask"
-                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        #             # tentukkan class label dan color yang akan kita gunakan membuat the bounding box and text
+        #         label = "Mask" if mask > withoutMask else "No Mask"
+        #         color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-                    # masukkan the probability pada label
-                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+        #             # masukkan the probability pada label
+        #         label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-                    # menampilkan label dan bounding box rectangle pada output frame
-                cv2.putText(frame, label, (startX, startY - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Ubah format frame menjadi RGB untuk Streamlit
-            stframe.image(frame, channels="RGB")  # Menampilkan frame dengan deteksi masker di area yang telah disediakan
+        #             # menampilkan label dan bounding box rectangle pada output frame
+        #         cv2.putText(frame, label, (startX, startY - 10),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        #         cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Ubah format frame menjadi RGB untuk Streamlit
+        #     stframe.image(frame, channels="RGB")  # Menampilkan frame dengan deteksi masker di area yang telah disediakan
 
         vid.release()
         cv2.destroyAllWindows()
@@ -157,7 +158,26 @@ def main():
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
 	image = frame.to_ndarray(format="bgr24")
-	return av.VideoFrame.from_ndarray(image, format="bgr24")
+        while True:
+		(locs, preds) = detect_and_predict_mask(image, faceNet, maskNet)
+                # loop lokasi wajah yang terdeteksi dan lokasi terkaitnya
+            	for (box, pred) in zip(locs, preds):
+                    	# unpack the bounding box and predictions
+	                (startX, startY, endX, endY) = box
+	                (mask, withoutMask) = pred
+	
+	                    # tentukkan class label dan color yang akan kita gunakan membuat the bounding box and text
+	                label = "Mask" if mask > withoutMask else "No Mask"
+	                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+	
+	                    # masukkan the probability pada label
+	                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+	
+	                    # menampilkan label dan bounding box rectangle pada output frame
+	                cv2.putText(image, label, (startX, startY - 10),
+	                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+	                cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+		return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 
 webrtc_ctx = webrtc_streamer(
